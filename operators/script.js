81f7f22d22afed30e85b37ede31af52b6dec59bf
Prashant Dumre <prasantdumre837@gmail.com>
2026-07-05 20:17:39 +0545

    Add files via upload

---
+++ operators/script.js
@@ -0,0 +1,41 @@
+//Arithmetic operators
+let b= 2;
+let c= 1;
+console.log("b+c=", b+c);
+console.log("b-c=", b-c);
+console.log("b*c=", b*c);
+console.log("b/c=", b/c);
+
+//Assignment operators
+let a= 4;
+a+= 5;
+console.log("a=", a);
+
+a*= 5;
+console.log("a=", a)
+
+a/= 2;
+console.log("a=", a)
+
+//comparison operators
+let d= 8;
+let e= 7;
+console.log("d==e", d == e);
+console.log("d!=e", d != e);
+console.log("d > e", d > e);
+
+
+let f=2;
+let g="2";
+console.log("f === g", f === g);
+console.log("f !== g", f !== g);
+
+//Logical operators
+let i=4;
+let j=6;
+
+console.log("cond1 && cond2", i > j && i === 4);
+
+console.log("cond1 || cond2", i < j || i===4);
+
+console.log("!(i > j)", !(i > j)); 
