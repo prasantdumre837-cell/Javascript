0013723e66d144a972df091971d55835e77f2943
Prashant Dumre <prasantdumre837@gmail.com>
2026-07-05 16:42:27 +0545

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+const student={
+    age: 20,
+    fullName: "Prashant",
+};
+console.log(student["age"]);
+console.log(typeof["fullName"]);
+
+student["age"]= student["age"]+1;
+console.log(student["age"]);
